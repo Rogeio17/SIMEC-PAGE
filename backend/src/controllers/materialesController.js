62304599dfce171787ad9d7f813466353111074e
Rogeio17 <rogercoss17@gmail.com>
2026-01-14 15:53:23 -0700

    Parche

--- backend/src/controllers/materialesController.js
+++ backend/src/controllers/materialesController.js
@@ -16,7 +16,6 @@ export async function listarMateriales(_req, res) {
          m.stock_actual, m.stock_minimo, m.ubicacion,
          m.activo, m.creado_en,
          m.proveedor_id,
-         p.nombre,
          p.nombre_comercial AS proveedor_nombre,
          m.ticket_numero, m.requiere_protocolo, m.protocolo_texto,
          m.precio_unitario,
